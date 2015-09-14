cf6d8f3770214ae1839e43fa47908f32e73e46db
Romeo Bellon <romeo.bellon@me.com>
2015-09-13 21:31:49 -0400

    Bootstrap 3

--- server/session.cpp
+++ server/session.cpp
@@ -146,7 +146,7 @@ void session::handle_read( const boost::system::error_code& error, size_t bytes_
           } catch (std::ios_base::failure e) {
               if (!params.empty()) {
                   entity_body.clear();
-                  entity_body = "<html><head><title>HTTP GET Request</title>"
+                  entity_body = "<!DOCTYPE html><html lang=\"en\"><head><title>HTTP GET Request</title>"
                           "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css\"></head>"
                           "<body><h1>GET Operation</h1><table class=\"table\" cellpadding=5 cellspacing=5 border=1>";
                   entity_body.append("<tr><td><b>Parameter</b></td><td><b>Value</b></td></tr>");
